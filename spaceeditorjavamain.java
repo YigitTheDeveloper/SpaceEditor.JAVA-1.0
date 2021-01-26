6a05eb96fec15c40625bf45b731b656755d7e179
Yiğit Efe Özdemir <77888730+YigitTheDeveloper@users.noreply.github.com>
2021-01-26 20:24:31 +0300

    spaceeditorjavaapp.java
    Just an aditional file that will help the program work!

---
+++ spaceeditorjavamain.java
@@ -0,0 +1,15 @@
+public class spaceeditorjavamain {
+
+	public static void main(String[] args) {
+		/***************************************************************
+		 * Name:      SpaceEditorJAVAmain.java
+		 * Purpose:   Code for Application Frame
+		 * Author:    Yiğit Efe Özdemir (yzdemr000@yahoo.com)
+		 * Created:   2020-10-08
+		 * Copyright: Yiğit Efe Özdemir (github.com/yigitthedeveloper)
+		 * License: Creative Commons
+		 **************************************************************/
+		new TextEditor();
+	}
+
+}
